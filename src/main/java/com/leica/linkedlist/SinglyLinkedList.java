b21dff3a9c5a57332e3282d49a964c6c7bb3a35e
leica1994 <705463446@qq.com>
2020-04-27 17:29:49 +0800

    add linked list structures algorithm

--- src/main/java/com/leica/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/leica/linkedlist/SinglyLinkedList.java
@@ -55,6 +55,104 @@ public class SinglyLinkedList {
     }
 
     /**
+     * insertion value at the beginning
+     *
+     * @param value the value to be inserting
+     */
+    public void insertToHead(int value) {
+        Node node = new Node(value, null);
+        insertToHead(node);
+    }
+
+    /**
+     * insert value at the beginning
+     *
+     * @param node the node to be inserting
+     */
+    public void insertToHead(Node node) {
+        if (head != null) {
+            node.next = head;
+        }
+        head = node;
+    }
+
+    /**
+     * insert value at the tailing
+     *
+     * @param value the value to be inserting
+     */
+    public void insertTail(int value) {
+        Node node = new Node(value, null);
+        if (head == null) {
+            head = node;
+        } else {
+            Node p = head;
+            while (p.next != null) {
+                p = p.next;
+            }
+            p.next = node;
+        }
+    }
+
+    /**
+     * insert the value after the node
+     *
+     * @param node  the node
+     * @param value the value
+     */
+    public void insertAfter(Node node, int value) {
+        Node p = new Node(value, null);
+        insertAfter(node, p);
+    }
+
+    /**
+     * insert the specified node p after the node
+     *
+     * @param node the node
+     * @param p    the node p
+     */
+    public void insertAfter(Node node, Node p) {
+        if (node == null) {
+            return;
+        }
+        p.next = node.next;
+        node.next = p;
+    }
+
+    /**
+     * insert the value before the node
+     *
+     * @param node  the node
+     * @param value the value
+     */
+    public void insertBefore(Node node, int value) {
+        Node p = new Node(value, null);
+        insertBefore(node, p);
+    }
+
+    /**
+     * insert the value before the node
+     *
+     * @param node the node
+     * @param p    the node p
+     */
+    public void insertBefore(Node node, Node p) {
+        if (node == null) {
+            return;
+        }
+        if (node == head) {
+            insertToHead(p);
+            return;
+        }
+        Node q = head;
+        while (q != null && q.next != node) {
+            q = q.next;
+        }
+        p.next = q.next;
+        q.next = p;
+    }
+
+    /**
      * singly linked list basic node
      * the node store int type data
      */
@@ -86,10 +184,10 @@ public class SinglyLinkedList {
 
     public static void main(String[] args) {
         Node node5 = new Node(5, null);
-        Node node4 = new Node(4,node5);
-        Node node3 = new Node(3,node4);
-        Node node2 = new Node(2,node3);
-        Node node1 = new Node(1,node2);
+        Node node4 = new Node(4, node5);
+        Node node3 = new Node(3, node4);
+        Node node2 = new Node(2, node3);
+        Node node1 = new Node(1, node2);
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
         singlyLinkedList.head = node1;
         Node node = singlyLinkedList.findByIndex(1);
