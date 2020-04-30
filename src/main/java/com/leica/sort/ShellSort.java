318e1a16549b19a7e034a3a1681ff8b8eda4c143
leica1994 <705463446@qq.com>
2020-04-30 22:25:45 +0800

    update ShellSort

--- src/main/java/com/leica/sort/ShellSort.java
+++ src/main/java/com/leica/sort/ShellSort.java
@@ -12,15 +12,19 @@ public class ShellSort {
         if (arr.length <= 1) {
             return;
         }
-        int step = arr.length - 1;
+        int step = arr.length >> 1;
         while (step >= 1) {
             for (int i = step; i < arr.length; i++) {
+                int value = arr[i];
                 int j = i - step;
-                if (arr[i] < arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                for (; j >= 0; j -= step) {
+                    if (value < arr[j]) {
+                        arr[j + step] = arr[j];
+                    } else {
+                        break;
+                    }
                 }
+                arr[j + step] = value;
             }
             step = step / 2;
         }
