58afb5a1cfa9f12aa0961e3730401a5c532b5ab3
leica1994 <705463446@qq.com>
2020-05-08 13:00:01 +0800

    update BinarySearch

--- src/main/java/com/leica/search/BinarySearch.java
+++ src/main/java/com/leica/search/BinarySearch.java
@@ -15,7 +15,7 @@ public class BinarySearch {
      * @param value the value to search
      * @return the index of the value to search if present
      */
-    public int search(int[] arr, int value) {
+    public int binarySearchNonRecursive(int[] arr, int value) {
         int low = 0;
         int high = arr.length - 1;
         while (low <= high) {
@@ -39,7 +39,7 @@ public class BinarySearch {
      * @param value the value to search
      * @return the index of the value to search if present
      */
-    public int binarySearch(int[] arr, int value) {
+    public int binarySearchRecursive(int[] arr, int value) {
         return binarySearchInternally(arr, 0, arr.length - 1, value);
     }
 
@@ -67,4 +67,114 @@ public class BinarySearch {
         }
     }
 
+    /**
+     * find the first element whose value is equal to the given value
+     * if there are duplicate values ​​in the array
+     *
+     * @param arr   the ordered array
+     * @param value the value to search
+     * @return the first element whose value is equal to the given value
+     */
+    public int binarySearchFirstElement(int[] arr, int value) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int middle = low + ((high - low) >> 1);
+            int midValue = arr[middle];
+            if (value < midValue) {
+                high = middle - 1;
+            } else if (value > midValue) {
+                low = middle + 1;
+            } else {
+                if (middle == 0 || arr[middle - 1] != midValue) {
+                    return middle;
+                } else {
+                    high = middle - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * find the last element whose value is equal to the given value
+     * if there are duplicate values ​​in the array
+     *
+     * @param arr   the ordered array
+     * @param value the value to search
+     * @return the last element whose value is equal to the given value
+     */
+    public int binarySearchLastElement(int[] arr, int value) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int middle = low + ((high - low) >> 1);
+            int midValue = arr[middle];
+            if (value < midValue) {
+                high = middle - 1;
+            } else if (value > midValue) {
+                low = middle + 1;
+            } else {
+                if (middle == arr.length - 1 || arr[middle + 1] != midValue) {
+                    return middle;
+                } else {
+                    high = middle - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * find the first element whose value is great or equal to the given value
+     *
+     * @param arr   the ordered array
+     * @param value the value to search
+     * @return the first element whose value is great or equal to the given value
+     */
+    public int binarySearchGreatOrEqualFirstElement(int[] arr, int value) {
+        int low = 0;
+        int high = arr.length;
+        while (low <= high) {
+            int middle = low + ((high - low) >> 1);
+            int midValue = arr[middle];
+            if (midValue >= value) {
+                if (middle == 0 || arr[middle - 1] < value) {
+                    return middle;
+                } else {
+                    high = middle - 1;
+                }
+            } else {
+                low = low + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     * find the last element whose value is great or equal to the given value
+     *
+     * @param arr   the ordered array
+     * @param value the value to search
+     * @return the last element whose value is great or equal to the given value
+     */
+    public int binarySearchGreatOrEqualLastElement(int[] arr, int value) {
+        int low = 0;
+        int high = arr.length;
+        while (low <= high) {
+            int middle = low + ((high - low) >> 1);
+            int midValue = arr[middle];
+            if (midValue > value) {
+                high = middle - 1;
+            } else {
+                if (middle == arr.length - 1 || arr[middle + 1] > value) {
+                    return middle;
+                } else {
+                    low = low + 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
